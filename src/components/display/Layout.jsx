3a81e0b89b8dd79e69832a6f2447e610fcebc922
Lautao <laucaballero50@gmail.com>
2023-04-09 19:46:55 -0300

    Dark mode added

--- src/components/display/Layout.jsx
+++ src/components/display/Layout.jsx
@@ -1,18 +1,35 @@
-import React from 'react'
-import Navbar from './Navbar'
-import Presentation from './Presentation'
-import Projects from './Projects'
-import Abilities from './Abilities'
-import Contact from './Contact'
+import React, { useEffect } from "react";
+import Navbar from "./Navbar";
+import Presentation from "./Presentation";
+import Projects from "./Projects";
+import Abilities from "./Abilities";
+import Contact from "./Contact";
+import useTheme from "../../components/hooks/darkTheme";
+import { BsFillSunFill, BsFillMoonFill } from "react-icons/bs";
 
 export default function Layout() {
+  const { changeDocumentTheme, theme, handleThemeButton } = useTheme();
+
+  useEffect(() => {
+    changeDocumentTheme();
+  }, [theme]);
+
   return (
-    <div className='min-h-screen bg-neutral-300'>
-      {/* <Navbar /> */}
+    <div className="min-h-screen bg-neutral-300 dark:bg-slate-800 dark:text-white">
+      <button
+        onClick={() => handleThemeButton()}
+        className="absolute right-[50px] top-[20px] lg:right-[150px] lg:top-[20px]"
+      >
+        {theme === "light" ? (
+          <BsFillSunFill className="w-[24px] h-[27px]" />
+        ) : (
+          <BsFillMoonFill className="w-[24px] h-[27px]" />
+        )}
+      </button>
       <Presentation />
       <Projects />
       <Abilities />
       <Contact />
     </div>
-  )
+  );
 }
--- src/components/display/Projects.jsx
+++ src/components/display/Projects.jsx
@@ -16,11 +16,19 @@ export default function Projects() {
         <div className="mt-5 lg:mt-0 lg:pl-4">
           <p className="text-lg font-bold">Open Task List!</p>
           <p className="text-xl">
-            Open Task List es un pequeño CRUD con la finalidad de ayudarte a
-            mantener un orden, pudiendo agregar, editar, eliminar o marcar como
-            completadas todas tus tareas.
+            Open Task List es una aplicación sencilla que te permite mantener un
+            registro de tus tareas, con la posibilidad de agregar, editar,
+            eliminar o marcarlas como completadas. Esta herramienta es ideal
+            para aquellos que buscan mantener un orden en su vida diaria y
+            desean maximizar su productividad. Con Open Task List, puedes tener
+            un mayor control sobre tus responsabilidades y organizar tus
+            actividades de manera efectiva.
           </p>
-          <a className="" href="https://opentasklist.vercel.app/">
+          <a
+            className=""
+            href="https://opentasklist.vercel.app/"
+            target="blank"
+          >
             <button className="mt-14 lg:ml-36 w-[200px] h-8 rounded-md shadow-lg bg-indigo-500 hover:bg-indigo-700">
               Ir al sitio
             </button>
@@ -36,11 +44,17 @@ export default function Projects() {
         <div className="lg:pl-4 mt-5 lg:mt-0">
           <p className="text-lg font-bold">Future Shop</p>
           <p className="text-xl">
-            Future Shop es un e-commerce realizado con una api publica que
-            cuenta con carrito de comprtas totalmente funcional, ademas de haber
-            sido creado con redux.
+            Future Shop es una plataforma de comercio electrónico construida a
+            partir de una API pública, la cual te ofrece un carrito de compras
+            completamente funcional. Este sitio web fue desarrollado utilizando
+            la tecnología de Redux, lo que garantiza una experiencia de usuario
+            fluida y sin interrupciones.
           </p>
-          <a className="" href="https://e-commerce-redux-nu.vercel.app/">
+          <a
+            className=""
+            href="https://e-commerce-redux-nu.vercel.app/"
+            target="blank"
+          >
             <button className="mt-14 lg:ml-36 w-[200px] h-8 rounded-md shadow-lg bg-indigo-500 hover:bg-indigo-700">
               Ir al sitio
             </button>
